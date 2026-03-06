be19e550b5a2d314dcbf51c0bb1b2744402bcb2f
VANSH001-OFFICIAL <vansh59rt@gmail.com>
2026-03-06 20:41:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,16 +7,15 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Mess
 
 # ===== CONFIG =====
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8677165936:AAH0D0urU-FRv3L0eL4BhxoWV8dIg7OL8Yw"
 
-ADMIN_ID = 123456789
+ADMIN_ID = 7117775366
 
 UPI_ID = "vansh59rt@fam"
 
 SUPPORT = "@black_seller16"
 
 ORDER_CHANNEL = "@blacksmm_payout"
-
 # ===== FLASK SERVER FOR RENDER =====
 
 app = Flask(__name__)
